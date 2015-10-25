c451b55400cb22ab6b619532ae7683c216c06b46
knalli <knallisworld@googlemail.com>
2015-10-25 18:51:08 +0100

    fix(docs): fix missing es

--- make-release.sh
+++ make-release.sh
@@ -53,7 +53,7 @@ pushd ../docs
 rm -rf {css,docs,data,font,img,js,partials,plato,de,en,fr,ru,uk,zh-cn,zh-tw,index.html,favicon.ico}
 mv ../angular-translate/site/* .
 cp -prf docs/en/* .
-cp -rf docs/{de,en,fr,ru,uk,zh-cn,zh-tw} .
+cp -rf docs/{de,en,es,fr,ru,uk,zh-cn,zh-tw} .
 git add -A
 git commit -m "Release $NEXT_RELEASE" .
 popd
